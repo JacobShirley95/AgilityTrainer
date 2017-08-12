0d7a6f4ac05b380a124505437cc030ad56bff867
Jacob Shirley <auriferous95@gmail.com>
2017-08-13 00:48:52 +0100

    First commit, not started yet.

---
+++ src/jaccob/agilitytrainer/AgilityTrainer.java
@@ -0,0 +1,16 @@
+package jaccob.agilitytrainer;
+
+import org.powerbot.script.PollingScript;
+import org.powerbot.script.Script.Manifest;
+import org.powerbot.script.rt4.ClientContext;
+
+@Manifest(name="AgilityTrainer", description="description", properties="")
+public class AgilityTrainer extends PollingScript<ClientContext> {
+
+	@Override
+	public void poll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
